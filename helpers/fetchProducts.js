298200c02091962b421cf93d44c8724844f7bf26
Bruce Alberto <brucealberto2@gmail.com>
2021-12-02 15:36:43 -0300

    analise

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,13 @@
-const fetchProducts = () => {
+const fetchProducts = async () => {
   // seu código aqui
-};
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+fetchProducts();
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
